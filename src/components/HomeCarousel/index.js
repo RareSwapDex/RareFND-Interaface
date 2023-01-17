ef24d2aa6f5ae59f133d348f305939aec735bed3
Jalil <benharkatdjalil@gmail.com>
2023-01-18 01:50:06 +0400

    test 2

--- src/components/HomeCarousel/index.js
+++ src/components/HomeCarousel/index.js
@@ -29,7 +29,7 @@ export default function HomeCarousel() {
 					>
 						<div>
 							<h1 style={{ fontWeight: "900", textAlign: "center" }}>
-								Rare Find Then Fundddd
+								Rare Find Then Funddddsssss
 							</h1>
 							<br />
 							<br />
